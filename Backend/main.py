fd88e3d2720b378efdd2418cc901c1a30b7401bf
vanessar810 <vanessaruiz810@gmail.com>
2026-01-07 00:16:08 -0500

    origins

--- Backend/main.py
+++ Backend/main.py
@@ -25,10 +25,11 @@ app = FastAPI(title="API Logística", description="API RESTful con JWT, CRUD, fi
 origins = [
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
+    "https://empresalogistica.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
